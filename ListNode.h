333173ce7fff8776a6e16d3c833bf45524f1ba1a
Уваров Николай <auvarovi.nikola2007@gmail.com>
2026-05-29 18:21:39 +0300

    ListNode.h

--- ListNode.h
+++ ListNode.h
@@ -7,14 +7,47 @@ namespace collections {
      */
     template<typename T>
     class ListNode {
-    public:
+    private:
         T value;
         ListNode<T>* nextNode;
 
+    public:
         /**
          * @brief Конструктор узла
          * @param val - значение для хранения в узле
          */
         explicit ListNode(const T& val) : value(val), nextNode(nullptr) {}
+
+        /**
+         * @brief Получение значения узла
+         * @return значение
+         */
+        T getValue() const {
+            return value;
+        }
+
+        /**
+         * @brief Установка значения узла
+         * @param val - новое значение
+         */
+        void setValue(const T& val) {
+            value = val;
+        }
+
+        /**
+         * @brief Получение указателя на следующий узел
+         * @return указатель на следующий узел
+         */
+        ListNode<T>* getNextNode() const {
+            return nextNode;
+        }
+
+        /**
+         * @brief Установка указателя на следующий узел
+         * @param node - указатель на следующий узел
+         */
+        void setNextNode(ListNode<T>* node) {
+            nextNode = node;
+        }
     };
 }
